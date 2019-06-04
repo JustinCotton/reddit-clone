8bb69632c021a5ffb0eadb812353030e1b673417
averastlast <carapowpowell@gmail.com>
2019-06-04 11:50:44 -0400

    took out comment

--- reddit_clone/models.py
+++ reddit_clone/models.py
@@ -7,18 +7,4 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
-
-
-# class Song(models.Model):
-#     title = models.CharField(max_length=100)
-#     album = models.CharField(max_length=100)
-#     preview_url = models.TextField(null=True, blank=True)
-#     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
-    
-#     def __str__(self):
-#         return self.title 
-
-# class Favorite(models.Model):
-#     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='favorites')
-#     song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name='favorites')
     
