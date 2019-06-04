371ad8026b55b30a2e420a6d5b4e3147a9afbe21
Justin D. Cotton <justin.d.cotton.esq@gmail.com>
2019-06-04 15:16:01 -0400

    All 3 models working in admin

--- reddit_clone/models.py
+++ reddit_clone/models.py
@@ -20,17 +20,6 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-class Post(models.Model):
-    created_at = models.TextField()
-    title = models.CharField(max_length=100)
-    content = models.CharField(max_length=100)
-    site_url = models.TextField(null=True, blank=True)
-    vote_total = models.IntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
-
-    def __str__(self):
-        return self.title
-
 class Comment(models.Model):
     created_at = models.DateField()
     content = models.TextField()
@@ -39,4 +28,4 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')
 
     def __str__(self):
-        return self.created_at
+        return self.content
