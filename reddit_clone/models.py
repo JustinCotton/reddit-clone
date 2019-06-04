7916bf6f79784ee8aabdb4c81ba9c7525e6f76d4
Justin D. Cotton <justin.d.cotton.esq@gmail.com>
2019-06-04 12:39:23 -0400

    Added Post model

--- reddit_clone/models.py
+++ reddit_clone/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    created_at = models.TextField()
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=100)
+    site_url = models.TextField(null=True, blank=True)
+    vote_total = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='songs')
+
+    def __str__(self):
+        return self.title
