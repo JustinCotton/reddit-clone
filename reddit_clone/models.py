70cc647d5fcc1ba810ff6a91c1eea10160000b42
Justin D. Cotton <justin.d.cotton.esq@gmail.com>
2019-06-04 14:23:44 -0400

    Added Post model, removed generics import from views

--- reddit_clone/models.py
+++ reddit_clone/models.py
@@ -8,6 +8,18 @@ class User(models.Model):
     def __str__(self):
         return self.username
     
+class Post(models.Model):
+    created_at = models.DateField()
+    title = models.CharField(max_length=100)
+    picture = models.CharField(max_length=100)
+    content = models.CharField(max_length=100)
+    site_url = models.TextField()
+    vote_total = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+
+    def __str__(self):
+        return self.title
+
 class Comment(models.Model):
     created_at = models.DateField()
     content = models.TextField()
--- reddit_clone/views.py
+++ reddit_clone/views.py
@@ -2,14 +2,6 @@ from rest_framework import viewsets
 from .serializers import PostSerializer, UserSerializer
 from .models import Post, Comment, User
 
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
